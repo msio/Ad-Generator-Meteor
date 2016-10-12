1c84e9a48ad54e792c246235b39a9107b14d0698
Michal Vranec <vranec.michal@gmail.com>
2016-10-12 22:33:32 +0200

    enable debug for templates, set permissions

--- both/collections.js
+++ both/collections.js
@@ -31,9 +31,11 @@ GeneratedAds = new FilesCollection({
 });
 
 AdTemplates = new FilesCollection({
-    debug: false,
+    debug: Meteor.isServer,
     collectionName: 'AdTemplates',
     storagePath: Meteor.isServer && Meteor.settings.private.adTemplatesPath,
+    permissions: 0777,
+    parentDirPermissions: 0777,
     allowClientCode: true,
     onBeforeUpload: function (file) {
         if (file.size > 1024 * 1024 * 10 || !/html/i.test(file.extension)) {
